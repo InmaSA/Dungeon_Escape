a845ad4796f86580f4fb9a8fd7d2b90496878a3b
Inma <inmanewmail@gmail.com>
2019-07-16 19:40:43 +0200

    pergamino y texto

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
+    let introSection = document.getElementById('intro')
+    introSection.setAttribute('class', 'disappear')
     game.init('canvas');
   };
 
